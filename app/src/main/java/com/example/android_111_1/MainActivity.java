0a9a95ac69bbdcd05d2424bee46baf233358192d
vic <h88001166@gmail.com>
2022-10-16 01:31:37 +0800

    Week2 Fibonacci Counter (homework)

--- app/src/main/java/com/example/android_111_1/MainActivity.java
+++ app/src/main/java/com/example/android_111_1/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count;
+    int count, number, temp0, temp1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +15,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         count = 0;
+        number = 0;
+        temp0 = 0;
+        temp1 = 0;
     }
 
     public void button0_click(View V) {
+        if (count == 1) {
+            temp1 = 1;
+        }
+        number = temp0 + temp1;
+        temp1 = temp0;
+        temp0 = number;
         count ++;
+
         TextView t = (TextView) findViewById(R.id.textView);
-        t.setText(Integer.toString(count));
+        t.setText(Integer.toString(number));
     }
 
     public void button1_click(View V) {
         count = 0;
+        number = 0;
+        temp0 = 0;
+        temp1 = 0;
+
         TextView t = (TextView) findViewById(R.id.textView);
-        t.setText(Integer.toString(count));
+        t.setText(Integer.toString(number));
     }
 }
