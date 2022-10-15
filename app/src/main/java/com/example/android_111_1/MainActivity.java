b0a8467e8133a35e7c31a04fbfe27ad1903cfca9
vic <h88001166@gmail.com>
2022-10-16 01:00:23 +0800

    Week2 Counter

--- app/src/main/java/com/example/android_111_1/MainActivity.java
+++ app/src/main/java/com/example/android_111_1/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.android_111_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = 0;
+    }
+
+    public void button0_click(View V) {
+        count ++;
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText(Integer.toString(count));
+    }
+
+    public void button1_click(View V) {
+        count = 0;
+        TextView t = (TextView) findViewById(R.id.textView);
+        t.setText(Integer.toString(count));
     }
 }
